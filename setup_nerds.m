b9ffc3835aad68528cd4428929696a6d843bcfd4
titipata <titipat.a@u.northwestern.edu>
2014-12-09 14:01:40 -0600

    Remove spgl1 from path

--- setup_nerds.m
+++ setup_nerds.m
@@ -1,22 +1,17 @@
 function setup_nerds
 %SETUP_NERDS include path for nerds
 
-path_temp = pwd;
-
 if isunix
     path(path);
     path(path, [pwd, '/utilities']);
-    path(path, [pwd, '/spgl1']);
     path(path, [pwd, '/spgl1_extend']);
 elseif ispc
     path(path);
     path(path, [pwd, '\utilities']);
-    path(path, [pwd, '\spgl1']);
     path(path, [pwd, '\spgl1_extend']);
 else
     path(path);
     path(path, [pwd, '/utilities']);
-    path(path, [pwd, '/spgl1']);
     path(path, [pwd, '/spgl1_extend']);
 end
 
