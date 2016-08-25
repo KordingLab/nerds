3a8f324ecaf01e989480a915d3e0b6415b8a5af9
evadyer <evadevah@gmail.com>
2016-08-25 09:42:08 -0700

    script to process bob data

---
+++ brain_observatory_data/script_bobdata_to_spikes.m
@@ -0,0 +1,37 @@
+% example on brain observatory data
+load('corrected_traces_exptcont_511509529_sessionA.mat')
+
+opts.numTrials = 1; % set to larger than 1 if you want to learn the template
+opts.thresh = 0.05;
+opts.L = 100;
+N = size(traces,1);
+
+telapsed = zeros(N,1);
+x_hat = cell(N,1);
+norm_traces = zeros(size(traces));
+%parpool(2)
+parfor i=1:N
+    flo = traces(i,:); 
+    flo = flo - min(flo);
+    flo = flo./max(flo);
+    norm_traces(i,:) = flo;
+    tstart = tic; 
+    [~, ~, x_hat{i}, ~] = compute_nerds(double(flo), opts); 
+    telapsed(i) = toc(tstart);
+    i,
+end
+
+X = zeros(size(traces));
+for i=1:N, 
+    X(i,:) = x_hat{i}; 
+end
+
+%figure; imagesc(X)
+%myColorMap = jet(256);
+%myColorMap(1:15, :) = ones(15,3); % Set row 128 to white.
+%colormap(myColorMap);
+%colorbar;
+
+save results-exptcont-511509529-sessionA.mat X telapsed norm_traces
+
+
